c7cde85fdcb61ce7b15ecfa5f0c0b4999c65b611
Yabooka <yabooka.rs@gmail.com>
2021-11-04 14:03:43 +0100

    added description

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -5,7 +5,10 @@ import BlogIndex from '@/components/blog/index'
 export default function BlogIndexPage({ articles, tags }) {
   return (
     <div className='container mx-auto'>
-      <Page title={'Yabooka | Blog'}>
+      <Page
+        title='Yabooka | Blog'
+        description='Saznajte više o popravkama i čestim problemima koji se javljaju kod Apple uređaja - iPhone 7 audio čip, iPhone servis Niš, Apple servis Niš'
+      >
         <BlogIndex articles={articles} tags={tags} />
       </Page>
     </div>
